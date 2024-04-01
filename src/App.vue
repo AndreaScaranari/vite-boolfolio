2226f8f489d72edc109dbafc071fc38653eac2ed
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-02 00:37:02 +0200

    imported_bootstrap

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'vite-boolfoglio'
+};
+</script>
+
+<template>
+    <main class="container">
+        <h1 class="text-center py-3">TEST</h1>
+    </main>
+</template>
+
+<style></style>
