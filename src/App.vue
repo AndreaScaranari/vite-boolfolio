a44192acc87a4c0eb76892ee833639cc511b4b4d
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-02 01:00:07 +0200

    api_working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
 
 export default {
     name: 'vite-boolfoglio',
     components: { AppHeader },
+    data: () => ({
+        projects: []
+    }),
+    methods: {
+        fetchProjects() {
+            axios.get(endpoint).then(res => {
+                // console.log(res.data);
+                this.projects = res.data;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
 };
 </script>
 
@@ -12,6 +28,11 @@ export default {
         <AppHeader />
         <main>
             <h1 class="text-center py-3">TEST</h1>
+            <ul>
+                <li v-for="project in projects" :key="project.id">
+                    <strong v-text="project.title"></strong>
+                </li>
+            </ul>
         </main>
     </div>
 </template>
