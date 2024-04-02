9067e9786b2f0e488f948043fb65e93672483658
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-02 03:08:59 +0200

    alert_added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppAlert from './components/AppAlert.vue';
 import ProjectList from './components/projects/ProjectList.vue';
 import axios from 'axios';
 const endpoint = 'http://localhost:8000/api/projects/';
 
 export default {
     name: 'vite-boolfoglio',
-    components: { AppHeader, ProjectList },
+    components: { AppHeader, ProjectList, AppAlert },
     data: () => ({
         projects: [],
         isLoading: false,
+        isAlertOpen: false,
     }),
     methods: {
         fetchProjects() {
@@ -17,11 +19,17 @@ export default {
             axios.get(endpoint).then(res => {
                 // console.log(res.data);
                 this.projects = res.data;
+                this.isAlertOpen = false;
             }).catch(err => {
                 console.error(err);
+                this.isAlertOpen = true;
             }).then(() => {
                 this.isLoading = false;
             })
+        },
+        closeErrorAlert() {
+            this.isAlertOpen = false;
+            // this.fetchProjects();
         }
     },
     created() {
@@ -35,6 +43,7 @@ export default {
         <AppHeader />
         <main>
             <h1 class="text-center py-3">TEST</h1>
+            <AppAlert :show="isAlertOpen" @close="closeErrorAlert" />
             <AppLoader v-if="isLoading" />
             <ProjectList v-else :projects="projects" />
         </main>
